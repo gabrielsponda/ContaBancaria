1f3f703c134ca8989b1c3419cc15fc7b472d6e66
Gabriel Sponda Freitas Bettarello <sponda.gabriel@aluno.ufabc.edu.br>
2023-11-17 08:47:53 -0300

    Aplicando mudanças visuais no menu.

--- src/contaBancaria/Menu.java
+++ src/contaBancaria/Menu.java
@@ -14,7 +14,6 @@ public class Menu {
 
 		while (true) {
 			
-			clear();
 			System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
 			System.out.println("┌──────────────────────────────────────┐");
 			System.out.println("|                                      |");
@@ -45,99 +44,99 @@ public class Menu {
 			switch (opcao) {
 			
 			case 1:
-
+				
 				System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
-				System.out.println("┌─────────────┐");
-				System.out.println("| CRIAR CONTA |");
-				System.out.println("├─────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
-				System.out.println("| // To Do    |");
-				System.out.println("└─────────────┘");
+				System.out.println("┌──────────────────────────────────────┐");
+				System.out.println("|             CRIAR CONTA              |");
+				System.out.println("├──────────────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
+				System.out.println("| // To Do                             |");
+				System.out.println("└──────────────────────────────────────┘");
 
 				break;
 
 			case 2:
 
 				System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
-				System.out.println("┌────────────────────────┐");
-				System.out.println("| LISTAR TODAS AS CONTAS |");
-				System.out.println("├────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
-				System.out.println("| // To Do               |");
-				System.out.println("└────────────────────────┘");
+				System.out.println("┌──────────────────────────────────────┐");
+				System.out.println("|        LISTAR TODAS AS CONTAS        |");
+				System.out.println("├──────────────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
+				System.out.println("| // To Do                             |");
+				System.out.println("└──────────────────────────────────────┘");
 
 				break;
 
 			case 3:
 
 				System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
-				System.out.println("┌─────────────────────────────────────┐");
-				System.out.println("| CONSULTAR DADOS DA CONTA POR NÚMERO |");
-				System.out.println("├─────────────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
-				System.out.println("| // To Do                            |");
-				System.out.println("└─────────────────────────────────────┘");
+				System.out.println("┌──────────────────────────────────────┐");
+				System.out.println("| CONSULTAR DADOS DA CONTA POR NÚMERO  |");
+				System.out.println("├──────────────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
+				System.out.println("| // To Do                             |");
+				System.out.println("└──────────────────────────────────────┘");
 
 				break;
 
 			case 4:
 
 				System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
-				System.out.println("┌──────────────────────────┐");
-				System.out.println("| ATUALIZAR DADOS DA CONTA |");
-				System.out.println("├──────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
-				System.out.println("| // To Do                 |");
-				System.out.println("└──────────────────────────┘");
+				System.out.println("┌──────────────────────────────────────┐");
+				System.out.println("|       ATUALIZAR DADOS DA CONTA       |");
+				System.out.println("├──────────────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
+				System.out.println("| // To Do                             |");
+				System.out.println("└──────────────────────────────────────┘");
 
 				break;
 
 			case 5:
 
 				System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
-				System.out.println("┌──────────────┐");
-				System.out.println("| APAGAR CONTA |");
-				System.out.println("├──────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
-				System.out.println("| // To Do     |");
-				System.out.println("└──────────────┘");
+				System.out.println("┌──────────────────────────────────────┐");
+				System.out.println("|            APAGAR CONTA              |");
+				System.out.println("├──────────────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
+				System.out.println("| // To Do                             |");
+				System.out.println("└──────────────────────────────────────┘");
 
 				break;
 
 			case 6:
 
 				System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
-				System.out.println("┌──────────┐");
-				System.out.println("| SAQUE    |");
-				System.out.println("├──────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
-				System.out.println("| // To Do |");
-				System.out.println("└──────────┘");
+				System.out.println("┌──────────────────────────────────────┐");
+				System.out.println("|                SAQUE                 |");
+				System.out.println("├──────────────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
+				System.out.println("| // To Do                             |");
+				System.out.println("└──────────────────────────────────────┘");
 
 				break;
 
 			case 7:
 
 				System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
-				System.out.println("┌──────────┐");
-				System.out.println("| DEPÓSITO |");
-				System.out.println("├──────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
-				System.out.println("| // To Do |");
-				System.out.println("└──────────┘");
+				System.out.println("┌──────────────────────────────────────┐");
+				System.out.println("|               DEPÓSITO               |");
+				System.out.println("├──────────────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
+				System.out.println("| // To Do                             |");
+				System.out.println("└──────────────────────────────────────┘");
 
 				break;
 
 			case 8:
 
 				System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
-				System.out.println("┌────────────────────────────┐");
-				System.out.println("| TRANSFERÊNCIA ENTRE CONTAS |");
-				System.out.println("├────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
-				System.out.println("| // To Do                   |");
-				System.out.println("└────────────────────────────┘");
+				System.out.println("┌──────────────────────────────────────┐");
+				System.out.println("|      TRANSFERÊNCIA ENTRE CONTAS      |");
+				System.out.println("├──────────────────────────────────────┤" + Cores.TEXT_BLACK + Cores.ANSI_WHITE_BACKGROUND);
+				System.out.println("| // To Do                             |");
+				System.out.println("└──────────────────────────────────────┘");
 
 				break;
 
 			default:
 
 				System.out.println(Cores.TEXT_WHITE_BOLD_BRIGHT + Cores.ANSI_BLUE_BACKGROUND );
-				System.out.println("┌─────────────────┐");
-				System.out.println("| OPÇÃO INVÁLIDA! |");
-				System.out.println("└─────────────────┘");
+				System.out.println("┌──────────────────────────────────────┐");
+				System.out.println("|           OPÇÃO INVÁLIDA!            |");
+				System.out.println("└──────────────────────────────────────┘");
 				
 				break;
 
@@ -156,11 +155,6 @@ public class Menu {
 		System.out.println("| github.com/gabrielsponda             |");
 		System.out.println("└──────────────────────────────────────┘");
 	
-	}
-
-	public static void clear() {
-		System.out.print("\033[H\033[2J");
-		System.out.flush();
 	}
 
 }
